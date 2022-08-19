19b2bd707c5edc9fc62a5baed5e7da31c1fc81ee
Yang Lun <724523660@qq.com>
2022-08-19 17:04:04 +0200

    modify

--- src/main/java/controllers/IndexView.java
+++ src/main/java/controllers/IndexView.java
@@ -1,4 +1,39 @@
 package controllers;
 
-public class IndexView {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import model.clientUser;
+import util.User;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IndexView implements Initializable{
+
+    @FXML
+    private Label accountId;
+
+    @FXML
+    private BorderPane chatInterface;
+
+    private Stage stage;
+
+    clientUser clientUser = new clientUser();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //get the userInfo from Server
+//        System.out.println("ssssss");
+//        var userInfo = clientUser.getUserInfo();
+//        System.out.println(userInfo.getFriendList());
+    }
+
 }
--- src/main/java/model/clientUser.java
+++ src/main/java/model/clientUser.java
@@ -2,6 +2,7 @@ package model;
 
 import org.tinylog.Logger;
 import util.Message;
+import util.User;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -11,20 +12,31 @@ import java.net.Socket;
 public class clientUser {
     private Socket socket;
 
+    public User userInfo;
+
+    public clientUser(){
+        try {
+            socket = new Socket("127.0.0.1",9999);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public boolean sendLoginInfo(Object o){
         boolean isSuccessLogin = false;
         try {
-            socket = new Socket("127.0.0.1",9999);
+//            socket = new Socket("127.0.0.1",9999);
             ObjectOutputStream oos=new ObjectOutputStream(socket.getOutputStream());
             oos.writeObject(o);
 
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             Message message = (Message) ois.readObject();
 
-            if(message.getMesType().equals("1")){
+            if(message.getMesType().equals("loginSuccess")){
                 Logger.info("登录成功");
                 isSuccessLogin = true;
-            } else if (message.getMesType().equals("2")) {
+//                userInfo = (User)ois.readObject();
+            } else if (message.getMesType().equals("loginFailed")) {
                 Logger.info("登录失败");
                 isSuccessLogin = false;
                 socket.close();
@@ -41,17 +53,17 @@ public class clientUser {
     public boolean sendRegisterInfo(Object o){
         boolean isSuccessLogin = false;
         try {
-            socket = new Socket("127.0.0.1",9999);
+//            Socket socket = new Socket("127.0.0.1",9999);
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             oos.writeObject(o);
 
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             Message message = (Message) ois.readObject();
 
-            if (message.getMesType().equals("1")) {
+            if (message.getMesType().equals("registerSuccess")) {
                 isSuccessLogin = true;
             }
-            else if(message.getMesType().equals("2")) {
+            else if(message.getMesType().equals("registerFailed")) {
                 isSuccessLogin = false;
                 socket.close();
             }
@@ -65,6 +77,23 @@ public class clientUser {
         }
 
         return isSuccessLogin;
+    }
+
+    public User getUserInfo(){
+        try {
+//            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+//            Message message = new Message();
+//            message.setMesType("userInfo");
+//            oos.writeObject(message);
+
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            User userInfo = (User)ois.readObject();
+            return userInfo;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
